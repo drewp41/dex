ecbfc90ded457eff215afe0dc76ff201beb6da12
drew <arpman41@gmail.com>
2023-02-04 14:20:10 -0800

    feat: init next repo

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from './page.module.scss'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <div>
+      dex
+    </div>
+  )
+}
