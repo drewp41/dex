1c4624e5b43d3531fd3343f7ced19cfa732fea43
drew <arpman41@gmail.com>
2023-02-05 16:47:59 -0800

    feat: tab metadata

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,10 +1,10 @@
 export default function Head() {
   return (
     <>
-      <title>Dex</title>
+      <title>Swap Aggregator</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Dex aggregator" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/favicon.svg" />
     </>
   )
 }
