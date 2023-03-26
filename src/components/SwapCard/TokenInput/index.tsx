689244f9324582e6d68b1b9541eb45a07010e827
drew <arpman41@gmail.com>
2023-03-25 23:18:15 -0700

    feat: swap card input formatting and more styling

--- src/components/SwapCard/TokenInput/index.tsx
+++ src/components/SwapCard/TokenInput/index.tsx
@@ -1,10 +1,35 @@
+import { useState } from 'react';
+import { ChevronDownIcon } from '@radix-ui/react-icons';
+
 import styles from '../index.module.scss';
 
 export default function TokenInput() {
+  const [val, setVal] = useState('');
+
+  const onValChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setVal(e.target.value);
+  };
+
+  const blockCertainChars = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'e' || e.key === '+' || e.key === '-') {
+      e.preventDefault();
+    }
+  };
+
   return (
     <div className={styles.inputGroup}>
-      <input className={styles.input} type='number' />
-      <button className={styles.tokenBtn}>ETH</button>
+      <input
+        className={styles.input}
+        placeholder='0'
+        type='number'
+        value={val}
+        onChange={onValChange}
+        onKeyDown={blockCertainChars}
+      />
+      <button className={styles.tokenBtn}>
+        ETH
+        <ChevronDownIcon />
+      </button>
     </div>
   );
 }
